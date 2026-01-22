a275dc80b1e1d27dcf5708ac44f70017228a125f
Edward Denis <edorward.21@gmail.com>
2026-01-22 09:40:04 +0300

    Added models, template and views

--- Mazuri/models.py
+++ Mazuri/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+class Customer (models.Model):
+    full_name = models.CharField(max_length = 100)
+    phone = models.CharField(max_length = 50)
 
-# Create your models here.
+class Order(models.Model):
+    STATUS_CHOICE=[
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('delivered', 'Delivered')
+    ]
+    oder_number = models.CharField(max_length=20,unique=True)
+    customer_name = models.ForeignKey(Customer, on_delete = models.CASCADE)
+    phone = models.CharField(max_length=50)
+
--- Mazuri/views.py
+++ Mazuri/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . models import Customers, Order
 
-# Create your views here.
+def Order_list(request):
+    maua = Order.object.all()
+    return render (request, 'list.html', {'maua': maua})
+
+def customer_list(request):
+    customer = Customers.object.all()
+    return render(request, 'list.html', {'customer': customer})
